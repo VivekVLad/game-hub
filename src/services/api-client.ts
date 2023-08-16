a9b4e121187e3fd8991c636aacdd42d9ea3375ce
VivyaPanari <ladvivek541@gmail.com>
2023-08-16 11:15:44 +0530

    Fetch Game Details

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -20,6 +20,9 @@ constructor(endpoint: string ) {
 }
  getAll = (config: AxiosRequestConfig ) => {
     return axiosInstance.get<FetchResponse<T>>(this.endpoint, config).then(res => res.data);
+};
+get = (id: number | string) => {
+    return axiosInstance.get<T>(this.endpoint + '/' + id ).then(res => res.data);
 }
 
 }
